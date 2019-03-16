5cb8c24cbde7f8a62b9192ab5c06a1b7dbc66136
ypfect <363825455@qq.com>
2019-03-16 22:54:46 +0800

    配置中心服务的搭建-完善服务中心和配置客户端的配置拉取

--- user-center/src/main/java/com/overstar/usercenter/controller/UserHelloController.java
+++ user-center/src/main/java/com/overstar/usercenter/controller/UserHelloController.java
@@ -17,8 +17,25 @@ public class UserHelloController {
     @Value("${server.port}")
     private int port;
 
+    @Value("${username}")
+    private String username;
+
+    @Value("${length}")
+    private int length;
+
     @GetMapping("/hello")
     public String helloUser(String userName){
         return userName+" how are you? this service form user service on port ->"+port;
+    }
+
+
+    @GetMapping("/configUsername")
+    public String configUsername(){
+        return "get remote config from config center service that length is -->" +length;
+    }
+
+    @GetMapping("/configLength")
+    public String configLength(){
+        return "get remote config from config center service that username is -->" +username;
     }
 }
